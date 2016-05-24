e5a119ca957d6244985871ea400accafa3995f0b
LEE ZHE YU <tzef8220@gmail.com>
2016-05-24 13:10:48 +0800

    update toggle button color, add flag array to keep flag status

--- BunbunuCustom/BunbunuCustom/Button/ToggleButton.swift
+++ BunbunuCustom/BunbunuCustom/Button/ToggleButton.swift
@@ -7,19 +7,27 @@
 //
 
 import UIKit
+import UIColor_Hex_Swift
+
+private let unFlagBgColor = UIColor(rgba: "#dcdce4")
+private let flagBGColor = UIColor.whiteColor()
+private let unFlagBorderColor = UIColor.blackColor()
+private let flagBorderColor = UIColor(rgba: "#53BC4A")
+private let unFlagIconColor = UIColor(rgba: "#999999")
+private let flagIconColor = UIColor(rgba: "#53BC4A")
 
 @IBDesignable
 class ToggleButton: StyleButton {
     @IBInspectable var flag: Bool = false {
         didSet {
-            self.borderColor = (flag ? UIColor.greenColor() : UIColor.blackColor())
-            self.bgColor = (flag ? UIColor.whiteColor() : UIColor.lightGrayColor())
+            self.borderColor = (flag ? flagBorderColor : unFlagBorderColor)
+            self.bgColor = (flag ? flagBGColor : unFlagBgColor)
             self.setNeedsDisplay()
         }
     }
     
     convenience init(flag: Bool, icon: UIImage?, title: String, clickClosure: (() -> ())?) {
-        self.init(borderWidth: 1, borderColor: UIColor.blackColor(), bgColor: UIColor.lightGrayColor(), textColor: UIColor.blackColor(), highlightBorderColor: nil, highlightBgColor: nil, highlightTextColor: nil, disabledColor: UIColor.lightGrayColor(), cornerRadius: CornerRadiusIntensity.Quarter, clickClosure: clickClosure)
+        self.init(borderWidth: 1, borderColor: (flag ? flagBorderColor : unFlagBorderColor), bgColor: (flag ? flagBGColor : unFlagBgColor), textColor: UIColor.blackColor(), highlightBorderColor: nil, highlightBgColor: nil, highlightTextColor: nil, disabledColor: UIColor.lightGrayColor(), cornerRadius: CornerRadiusIntensity.Quarter, clickClosure: clickClosure)
         self.clickClosure = clickClosure
         self.flag = flag
         self.iconImage = icon
@@ -44,13 +52,13 @@ class ToggleButton: StyleButton {
         flagIconPath.lineCapStyle = CGLineCap.Round
         flagIconPath.lineWidth = 3
         if flag {
-            UIColor.blackColor().setStroke()
+            flagIconColor.setStroke()
             flagIconPath.moveToPoint(CGPoint(x: CGRectGetMinX(flagIconRect), y: CGRectGetMinY(flagIconRect)))
             flagIconPath.addLineToPoint(CGPoint(x: CGRectGetMaxX(flagIconRect), y: CGRectGetMaxY(flagIconRect)))
             flagIconPath.moveToPoint(CGPoint(x: CGRectGetMaxX(flagIconRect), y: CGRectGetMinY(flagIconRect)))
             flagIconPath.addLineToPoint(CGPoint(x: CGRectGetMinX(flagIconRect), y: CGRectGetMaxY(flagIconRect)))
         } else {
-            UIColor.grayColor().setStroke()
+            unFlagIconColor.setStroke()
             flagIconPath.moveToPoint(CGPoint(x: CGRectGetMidX(flagIconRect), y: CGRectGetMinY(flagIconRect)))
             flagIconPath.addLineToPoint(CGPoint(x: CGRectGetMidX(flagIconRect), y: CGRectGetMaxY(flagIconRect)))
             flagIconPath.moveToPoint(CGPoint(x: CGRectGetMinX(flagIconRect), y: CGRectGetMidY(flagIconRect)))
--- BunbunuCustom/BunbunuCustom/Button/ToggleButtonView.swift
+++ BunbunuCustom/BunbunuCustom/Button/ToggleButtonView.swift
@@ -9,26 +9,37 @@
 import UIKit
 
 private let reuseIdentifier = "filterCell"
-class ToggleButtonView: UIView, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+class ToggleButtonView: UIView, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, ToggleButtonCellProtocol {
     @IBOutlet weak var collectionView: UICollectionView!
-    let filterArray = [
-        ("icon_list_like", "LIKE"),
-        ("icon_list_web", "WEB"),
-        ("icon_list_text", "TEXT"),
-        ("icon_list_namecard", "NAME CARD"),
-        ("icon_list_position", "POSITION"),
-        ("icon_list_email", "E-MAIL"),
-        ("icon_list_sms", "SMS"),
-        ("icon_list_phone", "PHONE")
+    var filterArray = [
+        ("icon_list_like", "LIKE", false),
+        ("icon_list_web", "WEB", false),
+        ("icon_list_text", "TEXT", false),
+        ("icon_list_namecard", "NAME CARD", false),
+        ("icon_list_position", "POSITION", false),
+        ("icon_list_email", "E-MAIL", false),
+        ("icon_list_sms", "SMS", false),
+        ("icon_list_phone", "PHONE", false)
     ]
     
     override func awakeFromNib() {
-        collectionView.registerClass(ButtonCell.classForCoder(), forCellWithReuseIdentifier: reuseIdentifier)
+        collectionView.registerClass(ToggleButtonCell.classForCoder(), forCellWithReuseIdentifier: reuseIdentifier)
+    }
+    
+    // MARK: Delegate
+    func chageFlagSatus(item: (String, String, Bool)) {
+        filterArray = filterArray.map({ (icon: String, title: String, flag: Bool) -> (String, String, Bool) in
+            var mapItem = (icon, title, flag)
+            if title == item.1 {
+                mapItem.2 = !flag
+            }
+            return mapItem
+        })
     }
     
     // MARK: UICollectionViewDelegateFlowLayout
     func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
-        let (_, title) = filterArray[indexPath.row]
+        let (_, title, _) = filterArray[indexPath.row]
         let size = title.stringSize([NSFontAttributeName: UIFont.systemFontOfSize(15)], size: CGSizeZero)
         return CGSize(width: 60 + size.width, height: 44)
     }
@@ -44,8 +55,9 @@ class ToggleButtonView: UIView, UICollectionViewDelegate, UICollectionViewDataSo
         return filterArray.count
     }
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
-        if let cell = collectionView.dequeueReusableCellWithReuseIdentifier(reuseIdentifier, forIndexPath: indexPath) as? ButtonCell {
+        if let cell = collectionView.dequeueReusableCellWithReuseIdentifier(reuseIdentifier, forIndexPath: indexPath) as? ToggleButtonCell {
             cell.configureCell(filterArray[indexPath.row])
+            cell.delegate = self
             return cell
         } else {
             return UICollectionViewCell()
@@ -53,10 +65,16 @@ class ToggleButtonView: UIView, UICollectionViewDelegate, UICollectionViewDataSo
     }
 }
 
-class ButtonCell: UICollectionViewCell {
-    func configureCell(item: (icon: String, title: String)) {
+protocol ToggleButtonCellProtocol: class {
+    func chageFlagSatus(item: (String, String, Bool))
+}
+class ToggleButtonCell: UICollectionViewCell {
+    weak var delegate: ToggleButtonCellProtocol?
+    func configureCell(item: (icon: String, title: String, flag: Bool)) {
         let size = item.title.stringSize([NSFontAttributeName: UIFont.systemFontOfSize(15)], size: CGSizeZero)
-        let button = ToggleButton(flag: false, icon: UIImage(named: item.icon), title: item.title, clickClosure: nil)
+        let button = ToggleButton(flag: item.flag, icon: UIImage(named: item.icon), title: item.title) {
+            self.delegate?.chageFlagSatus(item)
+        }
         button.frame = CGRectMake(0, 7, 60 + size.width, 30)
         for v in self.contentView.subviews {
             v.removeFromSuperview()
